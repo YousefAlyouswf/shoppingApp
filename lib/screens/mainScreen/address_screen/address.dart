467f82b04660becfc784897119b94ccc1fc28229
yousef <api.chc1989@gmail.com>
2020-07-21 14:55:54 -0500

    show icon to dismiss keyboard

--- lib/screens/mainScreen/address_screen/address.dart
+++ lib/screens/mainScreen/address_screen/address.dart
@@ -225,7 +225,26 @@ class _AddressState extends State<Address> {
   String cityFromSa = "";
   @override
   Widget build(BuildContext context) {
+    var keyboard = MediaQuery.of(context).viewInsets.bottom;
+
+    if (keyboard != 0.0) {
+      print("keyboard open");
+    } else {
+      print("keyboard closed");
+    }
     return new Scaffold(
+        floatingActionButton: keyboard != 0.0
+            ? FloatingActionButton(
+                onPressed: () {
+                  FocusScope.of(context).unfocus();
+                },
+                child: Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.white,
+                ),
+                backgroundColor: Colors.black,
+              )
+            : null,
         backgroundColor: Colors.white,
         appBar: new AppBar(
           backgroundColor: Colors.grey[200],
